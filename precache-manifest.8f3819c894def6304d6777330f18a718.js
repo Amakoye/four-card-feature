42bbdf2c9578aa9376aaf95c500fbff8e4ef00f5
Amakoye <charlesamakoye@gmail.com>
2020-06-03 16:09:18 +0300

    Updates

---
+++ precache-manifest.8f3819c894def6304d6777330f18a718.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6383eeb31c9a22d3630bf60c72afc1a4",
+    "url": "/four-card-feature/index.html"
+  },
+  {
+    "revision": "82f95d17139f8842b132",
+    "url": "/four-card-feature/static/css/main.8a89b5c0.chunk.css"
+  },
+  {
+    "revision": "188b748bbd7ed0780420",
+    "url": "/four-card-feature/static/js/2.306f52eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/four-card-feature/static/js/2.306f52eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f95d17139f8842b132",
+    "url": "/four-card-feature/static/js/main.e79df2a8.chunk.js"
+  },
+  {
+    "revision": "68d5b07a33ada01738a9",
+    "url": "/four-card-feature/static/js/runtime-main.2c58974d.js"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/four-card-feature/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/four-card-feature/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/four-card-feature/static/media/icon-supervisor.8f7824b3.svg"
+  },
+  {
+    "revision": "8a1a6a173b407f9e45ba99fb7f25f0db",
+    "url": "/four-card-feature/static/media/icon-team-builder.8a1a6a17.svg"
+  }
+]);
